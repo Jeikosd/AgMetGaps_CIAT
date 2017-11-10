f4f274767b5cf2cfc3348a5beabbc9985da6275e
aesquivel94 <a.esquivel@cgiar.org>
2017-11-10 12:00:28 -0500

    climatology for Temperature

--- planting_analysis.R
+++ planting_analysis.R
@@ -80,37 +80,30 @@ crop.time <- date_points %>%
   arrange(., group_by = season)
 
 
-# write.csv(x = crop.time, file = 'monthly_out/Rice_first.csv', row.names = NULL)
-
-
-
-
-
-##############################
-##############################
-# To use the spatial polygons information in the velox strack to points 
-sp_pdate <- rasterToPolygons(pdate,  dissolve = F) 
-
-
+write.csv(x = crop.time, file = 'monthly_out/Rice_first.csv')
 
 
 
 
 
 ##################################################################
-##########        Precipitation: Chirps                 ########## 
+##########      Functions to perform read raster,       ##########
+##########    convert to velox objects, extract points, ##########
+##########      convert to raster if is necessary.      ########## 
 ##################################################################
 
-# 'D:/AgMaps/Chips_Monthly/'
-## asignar la banda a que Año y mes pertenecen
-n_bands <- nc_open(paste0('Chips_Monthly/', 'chirps-v2.0.monthly.nc'))$dim$time$len
-n_bands <- n_bands - 8 # temporal
+
+###### function to read modificate raster to change band .y
+
+raster_mod <- function(.x, .y){
+  raster(.x, band = .y)
+}
 
 
 
 #### Create a function to strack the points 
 extract_velox <- function(velox_Object, points){
- 
+  
   coords <- coordinates(points) %>%
     tbl_df() %>%
     rename(lat = V1, long = V2)
@@ -125,14 +118,42 @@ extract_velox <- function(velox_Object, points){
 
 
 
-raster_mod <- function(.x, .y){
-  raster(.x, band = .y)
-}
+##############################
+##############################
+# To use the spatial polygons information in the velox strack to points 
+
+sp_pdate <- rasterToPolygons(pdate,  dissolve = F) 
+
+##############################
+##############################
+
+
+
+
+
+
+##################################################################
+##################################################################
+##########        Climate:  variability  analysis       ########## 
+##################################################################
+##################################################################
+
+
+
+##################################################################
+##########        Precipitation: Chirps                 ########## 
+##################################################################
+
+# 'D:/AgMaps/Chips_Monthly/'
+## asignar la banda a que Año y mes pertenecen
+n_bands <- nc_open(paste0('Chips_Monthly/', 'chirps-v2.0.monthly.nc'))$dim$time$len
+n_bands <- n_bands - 8 # temporal
+
 
 
 
 ## read monthly precipitation rasters with furure functions 
-plan(multisession, workers = availableCores() - 4)
+plan(multisession, workers = availableCores() - 3)
   
   
 system.time(  
@@ -153,10 +174,8 @@ system.time(
   
 
 
-
-
-#### proof1 extract points 
-plan(multisession, workers = availableCores() - 4)
+#### Extract points
+# plan(multisession, workers = availableCores() - 4)
 
 system.time(  
   dates_raster <- dates_raster %>% 
@@ -169,7 +188,7 @@ system.time(
 
 
 
-#### The idea of this function is make de summary of the one season
+#### Create Object to climate variability object. 
 
 
 time.levels <- crop.time %>% 
@@ -179,7 +198,9 @@ time.levels <- crop.time %>%
   as.tibble()
 
 
-# climatology function to calculate the climate variability in the crop season (mean, sd and cv)
+
+
+# climatology function to calculate the precipitation variability in the crop season (mean, sd and cv)
 
 climatology <-  function(levels){
 
@@ -218,19 +239,91 @@ cropV.prec <-  time.levels %>%
 
 # 'D:/AgMaps/GHCN_CAMS/'
 ## asignar la banda a que Año y mes pertenecen
-n_bands <- nc_open(paste0('D:/AgMaps/GHCN_CAMS/', 'data.nc'))$dim$T$len
+n_bands <- nc_open(paste0('Inputs/GHCN_CAMS/', 'data.nc'))$dim$T$len
 n_bands <- 36*12 # temporal
   
 
 # this data is available from this date, but we download to 1980 the same start-date to chirps 
 # nc_open(paste0('Inputs/GHCN_CAMS/', 'data.nc'))$dim$T$units 
+# for this we used the Chirps dates 
 
 
+# with future functions 
+# plan(multisession, workers = availableCores() - 3)
 
-GHCN_CAMS <- str_extract(nc_open(paste0('Chips_Monthly/', 'chirps-v2.0.monthly.nc'))$dim$time$units, "\\d{4}-\\d{1}-\\d{1}") %>%
+system.time(  
+  # read monthly temperature
+GHCN_CAMS <-   str_extract(nc_open(paste0('Chips_Monthly/', 'chirps-v2.0.monthly.nc'))$dim$time$units,
+              "\\d{4}-\\d{1}-\\d{1}") %>%
   as.Date() %>%
   seq(by = "month", length.out = n_bands)  %>%
   data_frame(date = .) %>%
   mutate(year = year(date), month = month(date)) %>%
-  mutate(band = 1:n_bands, file = rep(paste0('Inputs/GHCN_CAMS/', 'data.nc'), n_bands)) # %>%
+  mutate(band = 1:n_bands, file = rep(paste0('Inputs/GHCN_CAMS/', 'data.nc'), n_bands)) %>%
+  filter(month %in% crop.time$month)   %>%
+  mutate(load_raster = map2(.x = file, .y = band, .f = ~future(raster_mod(.x,.y)))) %>%
+  mutate(load_raster = map(.x = load_raster, .f = ~value(.x))) %>% 
+  mutate(raster_df = map(.x = load_raster, .f = ~future(velox(.x)))) %>%
+  mutate(raster_df = map(raster_df, .f = ~value(.x))) %>% 
+  mutate(points = map(.x = raster_df, .f = ~future(extract_velox(velox_Object = .x, points = sp_pdate)))) %>%
+  mutate(points = map(points, ~value(.x))) 
+)
+
+
+
+
+
+### with the object time.levels building the climatology function to temperature
+
+climatology_temp <-  function(levels){
   
+  data  <-  GHCN_CAMS %>%
+    mutate(., time = ifelse( 
+    month %in% crop.time$month[crop.time$season == as.character(levels)], 'TRUE' , 'FALSE')) %>%
+    filter(time == TRUE)   %>% 
+    select(year, month, points) %>% 
+    unnest  %>% 
+    mutate(valuesC =  values - 273.15) %>% 
+    group_by(year, lat, long )  %>% 
+    # convert Kelvin degrees to Centigrades
+    summarise(add =  mean(valuesC))  %>%
+    ungroup %>% 
+    group_by(lat, long)  %>%
+    summarise(mean.t =  mean(add), sd.t =  var(add))  %>%
+    mutate(cv.t = sd.t /mean.t * 100) 
+  
+  # nest(-year)
+  return(data)}
+
+
+cropV.temp <-  time.levels %>% 
+  mutate(data =  map(.x = value, .f = climatology_temp)) %>% 
+  unnest
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################################
+##################################################################
+##########        proof : first idea to analysis        ########## 
+##################################################################
+##################################################################
+
+
+
+inner_join(cropV.temp , cropV.prec)
+
+
+
+
+
+
+
