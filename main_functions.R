4d9a762751692b2e2896a4e4d9d7b2fa21c8c7f2
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-11-20 13:39:12 -0500

    making polygons from raster to use in velox package

--- main_functions.R
+++ main_functions.R
@@ -1,2 +1,311 @@
+
 ## main functions
 
+## funcion para crear puntos como poligonos (formato que necesita el paquete velox para extraer puntos de raster)
+
+# file_nc: sacks nc planting
+# out_path: folder to save the information
+# extent: extent if it is necessary to crop 
+# raster_source: source of the raster 
+# ...: to pass raster function
+
+raster_Polygons <- function(file_nc, out_path, extent, raster_source, ...){
+  
+  # file_nc <- paste0(path, planting)
+  # out_path 
+  # extent <- extent_information 
+  
+  crop_name <- stringr::str_extract(basename(file_nc), pattern = '[^.]+')
+  
+  calendar_points <- suppressWarnings(raster::raster(file_nc, ...))
+  # start_planting <- raster::raster(file_nc, varname = 'plant.start')
+  # end_planting <- raster::raster(file_nc, varname = 'plant.end')
+  
+  ### cropping with the climate information (chirps or agmerra) 
+  ### and make polygons for velox package
+  
+  points_df <- make_polygons(calendar_points, extent_information)
+  # setwd(out_path)
+  points_df <- sf::st_as_sf(points_df)
+  
+  # as(points_df, "Spatial")
+  
+  out_points <- mkdirs(fp = paste0(out_path, crop_name))
+  
+  st_write(points_df, dsn = out_points,
+           # layer = "rice_points_sacks.shp",
+           layer = paste0(crop_name, '_', raster_source, '.shp'),
+           driver = "ESRI Shapefile",
+           delete_layer = TRUE)
+           # delete_dsn = TRUE)
+}
+
+  
+
+## x:  a raster file
+## extent: extent to crop the raster file 
+
+make_polygons <- function(x, extent){
+  
+  temp_raster <- rasterTmpFile()
+  
+  exchange <- names(x)
+  variable <- stringr::str_replace_all(names(x), pattern = '[:punct:]+', replacement  = '_')
+  
+  points <- x %>%
+    raster::crop(extent) %>%
+    rasterToPoints() %>%
+    dplyr::tbl_df() %>%
+    dplyr::select(x, y) %>%
+    dplyr::rename(lat = x, long = y)
+  
+  polygons <- x %>%
+    raster::crop(extent) %>%
+    raster::writeRaster(filename = temp_raster, overwrite = TRUE) %>%
+    raster::rasterToPolygons(dissolve = F) 
+  
+  polygons@data = data.frame(polygons@data, data.frame(points)) %>%
+    dplyr::tbl_df() %>%
+    dplyr::rename(!!variable :=  !!exchange) %>%
+    select(lat, long, !!variable)
+  
+  
+  return(polygons)
+  # x <- plant_start
+  
+  
+}
+
+# fp: folder to make
+
+mkdirs <- function(fp) {
+  
+  if(!file.exists(fp)) {
+    mkdirs(dirname(fp))
+    dir.create(fp, showWarnings = F, mode = "777")
+    
+  }
+  
+  return(fp)
+} 
+
+  
+  
+  
+extract_date <- function(filename){
+  
+  # filename <- '/mnt/data_cluster_4/observed/gridded_products/chirps/daily//chirps-v2.0.1983.09.25.tif'
+  filename %>%
+    stringr::str_extract("\\d{4}.\\d{2}.\\d{2}") %>%
+    stringr::str_replace_all(pattern = '[:punct:]', replacement  = '-') %>%
+    as.Date(format = '%Y-%m-%d') %>%
+    magrittr::extract2(1)
+  
+}
+
+
+mean_point <- function(x){
+  
+  x[x<0] <- NA
+  mean(x, na.rm = T)
+  
+}
+
+
+velox_year <- function(data){
+  
+  data %>%
+    mutate(vx_raster = purrr::map(.x = file, .f = velox)) %>%
+    dplyr::select(vx_raster) %>%
+    magrittr::extract2(1)
+  
+}
+
+velox_year <- function(data){
+  
+  data %>%
+    # mutate(vx_raster = purrr::map(.x = file, .f = ~future(velox(.x)))) %>%
+    mutate(vx_raster = purrr::map(.x = file, .f = function(.x){
+      
+      y <- listenv()
+      y %<-% {
+        
+        velox(.x)
+      }
+      
+    })) %>%
+    # mutate(vx_raster = purrr::map(.x = vx_raster, .f = ~value)) %>%
+    dplyr::select(vx_raster) %>%
+    magrittr::extract2(1)
+  
+}
+
+
+
+extract_velox <- function(file, points){
+  
+  # r <- velox('/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily//chirps-v2.0.1981.01.01.tif')
+  # points <- plant_points
+  
+  
+  vx_raster <- velox(file)
+  
+  coords <- coordinates(points) %>%
+    tbl_df() %>%
+    rename(lat = V1, long = V2)
+  
+  # plan(multisession)
+  # ff_path <- paste0(tempfile(pattern = 'chirps_values'), '.ffdata')
+  
+  f <- future({
+    
+    vx_raster$extract(points, fun = mean) %>%
+      tbl_df() %>%
+      rename(precip = V1) %>%
+      dplyr::bind_cols(coords) %>%
+      dplyr::select(lat, long, precip) %>%
+      as.ffdf.data.frame()
+    # data.table
+  }
+  )
+  
+  # v <- value(f)  
+  return(f)
+  
+  
+  
+}
+
+
+
+extract_velox <- function(file, points){
+  
+  # r <- velox('/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily//chirps-v2.0.1981.01.01.tif')
+  # points <- plant_points
+  
+  vx_raster <- velox(file)
+  
+  
+  coords <- coordinates(points) %>%
+    tbl_df() %>%
+    rename(lat = V1, long = V2)
+  
+  date_raster <- extract_date(file) %>%
+    rep(dim(coords)[1])
+  date_raster <- data_frame(date_raster)
+  # plan(multisession)
+  # ff_path <- paste0(tempfile(pattern = 'chirps_values'), '.ffdata')
+  #   
+  values <- vx_raster$extract(points, fun = mean) %>%
+    tbl_df() %>%
+    rename(precip = V1) %>%
+    dplyr::bind_cols(coords, date_raster) %>%
+    dplyr::select(lat, long, precip, date)
+  # data.table
+  
+  
+  return(values)
+  
+  
+  
+}
+
+sack_to_points <- function(x){
+  
+  # x <- plant_start
+  exchange <- names(x)
+  variable <- stringr::str_replace_all(names(x), pattern = '[:punct:]+', replacement  = '_')
+  x <- rasterToPoints(x) %>%
+    tbl_df() %>%
+    rename(!!variable := !!exchange, 
+           lat = x, long = y)
+  
+  return(x)
+}
+
+make_date_information <- function(x){
+  
+  x %>%
+    mutate(year = lubridate::year(date),
+           month = lubridate::month(date),
+           day = lubridate::day(date))
+  
+}
+
+make_nest <- function(x){
+  
+  x %>%
+    nest()
+}
+
+velox_lst <- function(x){
+  
+  y <- listenv::listenv()
+  y <- future::future_lapply(x, FUN = velox) %>%
+    velox::velox()
+  
+  
+  
+}
+
+
+
+
+
+extract_velox <- function(file, points){
+  
+  # r <- velox('/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily//chirps-v2.0.1981.01.01.tif')
+  # points <- plant_points
+  
+  vx_raster <- velox(file)
+  
+  
+  coords <- sp::coordinates(points) %>%
+    tbl_df()
+  # 
+  date_raster <- extract_date(file) %>%
+    rep(dim(coords)[1])
+  date_raster <- data_frame(date_raster)
+  # 
+  # # plan(multisession)
+  # # ff_path <- paste0(tempfile(pattern = 'chirps_values'), '.ffdata')
+  # #
+  values <- vx_raster$extract(points, fun = mean_point) %>%
+    tbl_df() %>%
+    # dplyr::rename(precip = V1) %>%
+    dplyr::bind_cols(coords, date_raster) 
+  # data.table
+  
+  
+  # return(values)
+  
+  
+  
+}
+
+velox_lst <- function(x, points_coord){
+  
+  y <- listenv::listenv()
+  
+  y <- foreach::foreach(j = 1:length(x)) %dopar% {
+    
+    extract_velox(file = x[j], points = points_coord)
+  }
+  # y <- future::future_lapply(x, FUN = extract_velox, points = points_coord)
+  
+  y
+  
+}
+
+# make_joint <- function(x, y, ...){
+#   
+#   x <- x %>%
+#     mutate(lat = as.character(lat), 
+#            long = as.character(long))
+#   y <- y %>%
+#     mutate(lat = as.character(lat), 
+#            long = as.character(long))
+#   
+#   left_join(x, y, ...)
+# }  
+  
--- weather_analysis.R
+++ weather_analysis.R
@@ -1,4 +1,41 @@
-## weather analysis
+#####################################################################################
+##############  01 Make polygons from raster (to use in velox package) ##############
+#####################################################################################
+## Code to make points to extract using velox package
+## crops Rice, Maize, Trigo
+
+library(raster)
+library(sp)
+library(dplyr)
+library(sf)
+library(stringr)
+library(ncdf4)
+library(purrr)
+source("main_functions.R")
+
+path <- '/mnt/workspace_cluster_9/AgMetGaps/Inputs/05-Crop Calendar Sacks/'
+planting <- c('Rice.crop.calendar.nc', 'Maize.crop.calendar.nc', 'wheat.crop.calendar.nc')
+out_path <- '/mnt/workspace_cluster_9/AgMetGaps/weather_analysis/spatial_points/'    ## folder del proyecto para pegar la informacion necesaria
+extent_information <- c(-180,  180,  -50,   50)  ## extent with where is the information (chirps!!!)
+raster_source <- 'sacks'
+
+# raster_Polygons(file_nc = paste0(path, planting), out_path, extent = extent_information, raster_source)
+file_nc <- paste0(path, planting)
+purrr::map(.x = file_nc, .f = raster_Polygons, out_path, extent = extent_information, raster_source)
+
+rm(list = ls())
+
+
+###
+
+
+
+
+
+
+
+
+
 
 library(raster)
 library(velox)
@@ -7,76 +44,109 @@ library(dplyr)
 library(ncdf4)
 library(purrr)
 library(future)
+library(tidyr)
+library(lubridate)
 
-chirps_path <- '/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily/'
+# chirps_path <- '/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily/'
+chirps_path <- '/mnt/data_cluster_4/observed/gridded_products/chirps/daily/'
+  
 
 ## cargar raster por dia?
 ## Generar codigo tanto para extraer .tif como .nc
 ## tif solo puede contener un atributo, archivos .nc puede contener varios atributos
 
 raster_files <- list.files(chirps_path, pattern = '.tif$', full.names = T) %>%
-  data_frame(file = .)
+  data_frame(file = .) %>%
+  mutate(date = map(.x = file, .f = extract_date)) %>%
+  tidyr::unnest() %>%
+  mutate(year = lubridate::year(date)) 
 
+## hacer filtro solo hasta 2016 (o hasta donde se requiera)
 
-extract_date <- function(filename){
-  
-  stringr::str_extract(filename, "\\d{4}.\\d{2}.\\d{2}") %>%
-    stringr::str_replace_all(pattern = '[:punct:]', replacement  = '-') %>%
-    as.Date(format = '%Y-%m-%d') %>%
-    magrittr::extract2(1)
-}
+raster_files <- raster_files %>%
+  filter(year <= 2016) 
 
-extract_velox <- function(r, points){
-  
-  # r <- velox('/mnt/workspace_cluster_9/AgMetGaps/Chirps/daily//chirps-v2.0.1981.01.01.tif')
-  # points <- sp_pdate
-  
-  
-  coords <- coordinates(points) %>%
-    tbl_df() %>%
-    rename(lat = V1, long = V2)
-  
-  r$extract(points, fun = mean) %>%
-    tbl_df() %>%
-    rename(values = V1) %>%
-    bind_cols(coords) %>%
-    dplyr::select(lat, long, values)
-  
-}
 
 ### proof tiempo de demora sin paralelo con future
 
-system.time(
-  raster_files %>%
-    mutate(date = map(.x = file, .f = extract_date)) %>%
-    tidyr::unnest() %>%
-    mutate(velox_raster = map(.x = file, .f = velox))
-)
-user  system elapsed
-32.205  14.746  98.186
+# system.time(
+#   raster_files %>%
+#     mutate(date = map(.x = file, .f = extract_date)) %>%
+#     tidyr::unnest() %>%
+#     mutate(velox_raster = map(.x = file, .f = velox))
+# )
+# user  system elapsed
+# 32.205  14.746  98.186
 
-plan(multisession, workers = availableCores() - 3)
+# plan(multisession, workers = availableCores() - 3)  ## para trabajar en una sola maquina
+local_cpu <- rep("localhost", availableCores() - 3)
+external_cpu <- rep("climate.ciat.cgiar.org", 10)
 
+workers <- c(local_cpu, external_cpu)
+plan(cluster, workers = workers)
 
-system.time(
-y <- raster_files %>%
-  mutate(date = map(.x = file, .f = extract_date)) %>%
-  tidyr::unnest() %>%
+# system.time(
+chirps_df <- raster_files %>%
   mutate(velox_raster = map(.x = file, .f = ~future(velox(.x)))) %>%
   mutate(velox_raster = map(velox_raster, ~value(.x))) 
-)
+# )
 
-user  system elapsed
-10.370   6.186  52.743
+# user  system elapsed
+# 10.370   6.186  52.743
 
-pdate <- raster(paste0('/mnt/workspace_cluster_9/AgMetGaps/Inputs/05-Crop Calendar Sacks/Maize.crop.calendar.nc'), varname = 'plant.start') 
-sp_pdate <- rasterToPolygons(pdate,  dissolve = F) 
+## extraer puntos de planting date
+plant_start <- raster(paste0('/mnt/workspace_cluster_9/AgMetGaps/Inputs/05-Crop Calendar Sacks/Maize.crop.calendar.nc'), varname = 'plant.start')
+plant_end <- raster(paste0('/mnt/workspace_cluster_9/AgMetGaps/Inputs/05-Crop Calendar Sacks/Maize.crop.calendar.nc'), varname = 'plant.end')
+  
+plant_points <- rasterToPolygons(plant_start,  dissolve = F) 
 
-y <- y %>%
+chirps_df <- chirps_df %>%
   # mutate(points = map2(.x = velox_raster, .y = sp_pdate, .f = extract_velox))
-  mutate(points = map(.x = velox_raster, .f = ~future(extract_velox(r = .x, points = sp_pdate)))) %>%
+  mutate(points = map(.x = velox_raster, .f = ~future(extract_velox(r = .x, points = plant_points)))) %>%
   mutate(points = map(points, ~value(.x))) 
 
+plant_start <- sack_to_points(plant_start)
+plant_end <- sack_to_points(plant_end)
 
-select(y, date, points) %>%
-  unnest()
+select(chirps_df, date, points) %>%
+  unnest() %>%
+  purrr::invoke_map(.f = left_join, .x = list(plant_start, plant_end), by = c('lat', 'long'))
+  left_join(plant_start, by = c('lat', 'long'))
+  
+  parallel::stopCluster(cl)
+
+
+x <- function(){
+  
+  length_matrix <- scan(file=stdin(), what=integer(), nlines=1)
+  
+  if (length_matrix >= 100 | length_matrix <= -100) {
+    stop("Invalid N by the hackerrank Constraints")
+  }
+  
+  A <- matrix(scan(file=stdin(), nlines = length_matrix), length_matrix, length_matrix, byrow = TRUE)
+  
+  diag1 <- sum(diag(A))
+  
+  
+  diag2 <- sum(diag(apply(A,2,rev))) 
+  
+  result <- abs(diag1 - diag2)
+  print(result)
+  return(result)
+  
+}
+
+x()
+
+workers <- c("climate.ciat.cgiar.org", "climate.ciat.cgiar.org", "climate.ciat.cgiar.org", "climate.ciat.cgiar.org", "climate.ciat.cgiar.org",
+             "localhost", "localhost", "localhost")
+cl <- makeClusterPSOCK(workers, revtunnel = TRUE, outfile = "")
+
+
+f <- list()
+for (i in 1:10000) {
+  f[[i]] <- future({ rnorm(i, mean = mu, sd = sigma) })
+}
+v <- values(f)
+
