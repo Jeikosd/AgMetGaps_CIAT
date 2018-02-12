4bcfcb0bbbbde8c94210494e82f5fffbe994c459
Jeikosd <jeison.mesa@correounivalle.edu.co>
2018-02-12 16:22:46 -0500

    making gaps and potential yield from Iizumi

--- make_gaps.R
+++ make_gaps.R
@@ -25,12 +25,20 @@ bind_file <- list.dirs(climate_binds, recursive = FALSE) %>%
   pull(path)
 
 bind_file <- purrr::map(.x = bind_file, .f = list.files, full.names = T, pattern = '*BinMatrix.nc$')
+bind_raster <- purrr::map(.x = bind_file, .f = raster::raster)
 
-Iizumi_raster <- purrr::map(.x = yield_file[[1]], .f = raster::raster)
-bind_raster <- raster(bind_file[[1]])
+library(future)
+library(future.apply)
+plan(list(tweak(multisession, workers = 3), tweak(multisession, workers = 4)))
 
-## hacer esto en paralelo
-purrr::map(.x = Iizumi_raster, .f = raster::resample, bind_raster)
+Iizumi_raster <- purrr::map(.x = yield_file, .f = ~future(fraster(.x))) %>%
+  future::values()
+
+
+
+resample_Iizumi <- purrr::map(.x = Iizumi_raster, .f = ~future(fresample(.x, bind_raster[[1]]))) %>%
+  future::values()
+
 
 
 
@@ -38,3 +46,91 @@ purrr::map(.x = Iizumi_raster, .f = raster::resample, bind_raster)
 bind_raster <- purrr::map(.x = Iizumi_raster, .f = raster)
 
 z <- resample(Iizumi_raster, bind_raster)
+
+
+
+
+
+
+
+
+#### funciones para make_gaps
+
+## x: files to load raster
+
+fraster <- function(x){
+  
+  x <- future.apply::future_lapply(X = x, FUN = raster)
+  
+}
+
+## x: raster
+## y: resolution (to make the resample)
+fresample <- function(x, y){
+  
+  x <- future.apply::future_lapply(X = x, FUN = resample, y)
+  
+}
+
+
+# x: yield
+# y: contenedor climatico
+
+potential <- function(x, y){
+  
+  x <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
+    rotate %>%
+    velox()
+  
+  y <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/Yield_Gaps_ClimateBins/maize_yieldgap_netcdf/YieldGap_maize_2000_BaseGDD_8_MaxYieldPct_95_ContourFilteredClimateSpace_10x10_prec_BinMatrix.nc') 
+  
+  # x <- raster('/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
+  # rotate %>%
+  # velox()
+  # y <- raster('/mnt/workspace_cluster_9/AgMetGaps/Inputs/Yield_Gaps_ClimateBins/maize_yieldgap_netcdf/YieldGap_maize_2000_BaseGDD_8_MaxYieldPct_95_ContourFilteredClimateSpace_10x10_prec_BinMatrix.nc') 
+  
+  # r3 <- overlay(x, y, fun=function(x,y){return(y)})
+  points_bind <- rasterToPoints(y) %>%
+    tbl_df() %>%
+    sf::st_as_sf(coords = c("x","y"))
+  
+  z <- x$extract_points(points_bind) %>%
+    tbl_df() %>% 
+    rename(yield = V1) 
+  # z <- raster::extract(x, points_bind[, 1:2]) %>%
+  #   tbl_df()
+  
+  p <- bind_cols(st_coordinates(points_bind) %>% tbl_df(), z, points_bind) %>%
+    dplyr::mutate(BinMatrix = as.factor(BinMatrix))
+  
+  pot <- p %>%
+    group_by(BinMatrix) %>%
+    summarise(potential = quantile(yield, probs = 0.95, na.rm =  TRUE))
+  
+  pot_iizumi <- left_join(p, pot, by = 'BinMatrix') %>%
+    dplyr::mutate(potential = ifelse(is.na(yield), NA, potential)) %>%
+    dplyr::mutate(potential2 = case_when()) 
+  dplyr::mutate(gap = potential - yield)
+  
+  ## rasterize
+  m <- x$as.RasterLayer(band = 1)
+  
+  coords <- pot_iizumi %>%
+    select(X, Y) %>%
+    data.frame 
+  
+  potential <- pot_iizumi %>%
+    # dplyr::select(!!var) %>%
+    dplyr::select(potential) 
+  
+  gap <- pot_iizumi %>%
+    # dplyr::select(!!var) %>%
+    dplyr::select(gap) %>%
+    pull
+  
+  na.omit(pot_iizumi)
+  potential <- rasterize(coords, m, potential, fun = mean)
+  
+  plot(potential)
+  
+}
