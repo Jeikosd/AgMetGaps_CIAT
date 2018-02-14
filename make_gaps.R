819ecf183f8284e9ea0a02168025b7b8e6ba325a
Jeikosd <jeison.mesa@correounivalle.edu.co>
2018-02-14 14:18:34 -0500

    adding out path yield gap and potential

--- make_gaps.R
+++ make_gaps.R
@@ -31,36 +31,46 @@ library(future)
 library(future.apply)
 plan(list(tweak(multisession, workers = 3), tweak(multisession, workers = 4)))
 
+## es necesario rotar la informacion de Iizumi
 Iizumi_raster <- purrr::map(.x = yield_file, .f = ~future(fraster(.x))) %>%
   future::values()
 
-
-
+## para que la informacion quede a la misma resolucion de los bind climaticos
 resample_Iizumi <- purrr::map(.x = Iizumi_raster, .f = ~future(fresample(.x, bind_raster[[1]]))) %>%
   future::values()
 
 
+## Nombres de los potencial y gap
+library(stringr)
 
 
 
-bind_raster <- purrr::map(.x = Iizumi_raster, .f = raster)
-
-z <- resample(Iizumi_raster, bind_raster)
+# purrr::map(.x = yield_file, .f = str_extract, pattern = "^[^.]*")
 
 
+# str_extract(yield_file[[1]], pattern = "^[^.]*")
 
+# str_extrac(yield_file[[1]], pattern = '.nc4', "")
 
-
-
+out_potential <- purrr::map(.x = yield_file, .f = out_names, '_potential.tif')
+out_gap <- purrr::map(.x = yield_file, .f = out_names, '_gap.tif')
 
 
 #### funciones para make_gaps
 
 ## x: files to load raster
 
+rotate_raster <- function(x){
+  
+  x <- raster(x) %>%
+    raster::rotate(x)
+}
+
 fraster <- function(x){
   
-  x <- future.apply::future_lapply(X = x, FUN = raster)
+  
+  
+  x <- future.apply::future_lapply(X = x, FUN = rotate_raster)
   
 }
 
@@ -73,14 +83,26 @@ fresample <- function(x, y){
 }
 
 
+##
+out_names <- function(x, type){
+  
+  # x <- "/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi//maize_major/yield_1981"
+  
+  glue("{str_extract(x, pattern = '^[^.]*')}{type}")
+   
+
+  
+}
+
 # x: yield
 # y: contenedor climatico
 
+
 potential <- function(x, y){
   
-  x <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
-    rotate %>%
-    velox()
+  # x <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
+    # rotate %>%
+    # velox()
   
   y <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/Yield_Gaps_ClimateBins/maize_yieldgap_netcdf/YieldGap_maize_2000_BaseGDD_8_MaxYieldPct_95_ContourFilteredClimateSpace_10x10_prec_BinMatrix.nc') 
   
@@ -132,7 +154,9 @@ potential <- function(x, y){
   na.omit(pot_iizumi)
   potential <- rasterize(coords, y, potential, fun = mean)
   gap <- rasterize(coords, y, gap, fun = mean)
-  writeRaster(gap, filename="/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/gap_1981_maize.tif", format="GTiff", overwrite=TRUE)
+  
+  writeRaster(potential, filename="/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/gap_1981_maize.tif", format = "GTiff", overwrite = TRUE)
+  writeRaster(gap, filename="/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/gap_1981_maize.tif", format = "GTiff", overwrite = TRUE)
 
   
 }
