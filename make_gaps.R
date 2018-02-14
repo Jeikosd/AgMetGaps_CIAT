7629fd20c0554a5db8d4266a554f020863a10156
Jeikosd <jeison.mesa@correounivalle.edu.co>
2018-02-14 14:33:46 -0500

    fix bug with resampling method

--- make_gaps.R
+++ make_gaps.R
@@ -40,6 +40,9 @@ resample_Iizumi <- purrr::map(.x = Iizumi_raster, .f = ~future(fresample(.x, bin
   future::values()
 
 
+resample_Iizumi <- purrr::map2(.x = Iizumi_raster, .y = bind_raster, .f = ~future(fresample(.x, .y))) %>%
+  future::values()
+
 ## Nombres de los potencial y gap
 library(stringr)
 
@@ -98,13 +101,16 @@ out_names <- function(x, type){
 # y: contenedor climatico
 
 
-potential <- function(x, y){
+make_gap <- function(x, y){
+  
+  # x <- resample_Iizumi[[1]][1]
+  # y <- 
   
   # x <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
     # rotate %>%
     # velox()
   
-  y <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/Yield_Gaps_ClimateBins/maize_yieldgap_netcdf/YieldGap_maize_2000_BaseGDD_8_MaxYieldPct_95_ContourFilteredClimateSpace_10x10_prec_BinMatrix.nc') 
+  # y <- raster('//dapadfs/workspace_cluster_9/AgMetGaps/Inputs/Yield_Gaps_ClimateBins/maize_yieldgap_netcdf/YieldGap_maize_2000_BaseGDD_8_MaxYieldPct_95_ContourFilteredClimateSpace_10x10_prec_BinMatrix.nc') 
   
   # x <- raster('/mnt/workspace_cluster_9/AgMetGaps/Inputs/iizumi/maize/yield_1981.nc4') %>%
   # rotate %>%
@@ -116,20 +122,20 @@ potential <- function(x, y){
     tbl_df() %>%
     sf::st_as_sf(coords = c("x","y"))
 
-  z <- x$extract_points(points_bind) %>%
+  yield <- x$extract_points(points_bind) %>%
     tbl_df() %>% 
     rename(yield = V1) 
   # z <- raster::extract(x, points_bind[, 1:2]) %>%
   #   tbl_df()
   
-  p <- bind_cols(st_coordinates(points_bind) %>% tbl_df(), z, points_bind) %>%
+  yield_by_bind <- bind_cols(st_coordinates(points_bind) %>% tbl_df(), yield, points_bind) %>%
     dplyr::mutate(BinMatrix = as.factor(BinMatrix))
   
-  pot <- p %>%
+  pot <- yield_by_bind %>%
     group_by(BinMatrix) %>%
     summarise(potential = quantile(yield, probs = 0.95, na.rm =  TRUE))
   
-  pot_iizumi <- left_join(p, pot, by = 'BinMatrix') %>%
+  pot_iizumi <- left_join(yield_by_bind, pot, by = 'BinMatrix') %>%
     # dplyr::mutate(potential = ifelse(is.na(yield), NA, potential)) %>%
     dplyr::mutate(potential = if_else(is.na(yield), NA_real_, potential)) %>%
     # dplyr::mutate(potential = case_when( is.na(yield) == NA_real_ ~ NA_real_, TRUE ~ potential))
